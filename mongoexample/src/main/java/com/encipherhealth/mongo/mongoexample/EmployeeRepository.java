743749936bf3fe96e9ba37da898aaf2a6b52282a
Mohanraj S <147261507+mohanraj1203@users.noreply.github.com>
2024-12-19 16:12:49 +0530

    Delete mongoexample directory

--- mongoexample/src/main/java/com/encipherhealth/mongo/mongoexample/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.encipherhealth.mongo.mongoexample;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EmployeeRepository extends MongoRepository<Employee, Integer> {
-}
